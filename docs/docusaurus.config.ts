1695ede90ae751cba06749f52e79416cf8325de6
Arnaud Bailly <arnaud@pankzsoft.com>
2025-04-08 09:16:08 +0200

    Update blog title

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Tartarus',
+  title: 'CF/Antithesis',
   tagline: "ch'i' non ti lascerò nel mondo basso.",
   favicon: 'img/cardano-ada-logo.svg',
 

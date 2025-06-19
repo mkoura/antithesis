7e150fc8b46aa8d089299d2f870f9eaf088a901c
Johannes Lund <anviking@me.com>
2025-06-19 17:36:40 +0200

    fixes with overlays

--- cli/flake.nix
+++ cli/flake.nix
@@ -7,16 +7,30 @@
     nixpkgs.follows = "haskellNix/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-utils.url = "github:hamishmack/flake-utils/hkm/nested-hydraJobs";
+    iohkNix = {
+      url = "github:input-output-hk/iohk-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     CHaP = {
       url = "github:intersectmbo/cardano-haskell-packages?ref=repo";
       flake = false;
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, haskellNix, CHaP, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, haskellNix, CHaP, iohkNix, ... }:
     let
       lib = nixpkgs.lib;
       version = self.dirtyShortRev or self.shortRev;
+
+      fix-blst = final: prev: {
+        haskell-nix = prev.haskell-nix // {
+          extraPkgconfigMappings = prev.haskell-nix.extraPkgconfigMappings // {
+            # String pkgconfig-depends names are mapped to lists of Nixpkgs
+            # package names
+            "libblst" = [ "blst" ];
+          };
+        };
+      };
 
       perSystem = system:
         let
@@ -33,7 +47,12 @@
           };
 
           pkgs = import nixpkgs {
-            overlays = [ haskellNix.overlay ];
+            overlays = [
+              iohkNix.overlays.crypto # modified crypto libs
+              iohkNix.overlays.cardano-lib
+              haskellNix.overlay # some functions
+              fix-blst
+            ];
             inherit system;
           };
           indexState = "2025-05-07T00:00:00Z";

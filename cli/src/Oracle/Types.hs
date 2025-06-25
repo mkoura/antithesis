a5e8842720e686cf8ff242cb93c0d7a76378ebb9
Pawel Jakubas <jakubas.pawel@gmail.com>
2025-06-25 11:34:40 +0200

    add types
    rebase twaeks

--- cli/src/Oracle/Types.hs
+++ cli/src/Oracle/Types.hs
@@ -1,5 +1,46 @@
 {-# LANGUAGE StrictData #-}
 
 module Oracle.Types
-    (
+    ( RequestRefs (..)
+    , MPFSRequest (..)
     ) where
+
+import Core.Types (Key, Operation, OutputReference, Owner)
+import Data.Aeson
+    ( FromJSON (parseJSON)
+    , KeyValue ((.=))
+    , ToJSON (toJSON)
+    , object
+    , withObject
+    , (.:)
+    )
+
+newtype RequestRefs = RequestRefs
+    { outputReferences :: [OutputReference]
+    }
+    deriving (Eq, Show)
+
+instance ToJSON RequestRefs where
+    toJSON (RequestRefs{outputReferences}) =
+        object
+            [ "requestIds" .= outputReferences
+            ]
+instance FromJSON RequestRefs where
+    parseJSON = withObject "Requests" $ \v ->
+        RequestRefs
+            <$> v .: "requestIds"
+
+data RequestChange = RequestChange
+    { owner :: Owner
+    , key :: Key
+    , value :: Operation
+    }
+    deriving (Eq, Show)
+
+
+data MPFSRequest = MPFSRequest
+    { outputRefId :: OutputReference
+    , change :: RequestChange
+    , owner :: Owner
+    }
+    deriving (Eq, Show)

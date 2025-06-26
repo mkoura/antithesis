8dd86c9629ac7bf44240d6ac804c5d380547d871
Pawel Jakubas <jakubas.pawel@gmail.com>
2025-06-26 21:40:52 +0200

    first logic impl

--- cli/src/Oracle/Validate/Cli.hs
+++ cli/src/Oracle/Validate/Cli.hs
@@ -1,24 +1,53 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Oracle.Validate.Cli
     ( validateCmd
     , ValidateCommand (..)
     ) where
 
+import Control.Monad.IO.Class
+    ( liftIO
+    )
 import Core.Types
     ( TokenId
     )
 import MPFS.API
     ( getToken
     )
+import MPFS.Types
+    ( MPFSGetToken
+    , MPFSRequest (..)
+    , MPFSRequestChange (..)
+    , MPFSTokenState (..)
+    )
 import Servant.Client (ClientM)
-import Text.JSON.Canonical (JSValue)
+import Text.JSON.Canonical
+    ( FromJSON (..)
+    , JSValue
+    , ReportSchemaErrors (..)
+    )
+
+import MPFS.Types qualified as MPFS
 
 data ValidateCommand
     = ValidateRequests
     deriving (Eq, Show)
 
+instance ReportSchemaErrors ClientM where
+    expected expct (Just got) =
+        liftIO
+            $ fail
+            $ "Expected: "
+                ++ expct
+                ++ ", but got: "
+                ++ got
+    expected expct Nothing = liftIO $ fail $ "Expected: " ++ expct
+
 validateCmd :: TokenId -> ValidateCommand -> ClientM JSValue
 validateCmd tk command = do
     case command of
         ValidateRequests -> do
-            _canonicalJSON <- getToken tk
+            canonicalJSON <- getToken tk
+            resp <- fromJSON @_ @MPFSGetToken canonicalJSON
+            let requestsToValidate = MPFS.requests resp
             undefined
--- cli/src/Oracle/Validate/Logic.hs
+++ cli/src/Oracle/Validate/Logic.hs
@@ -1,12 +1,26 @@
 {-# LANGUAGE StrictData #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
 
 module Oracle.Validate.Logic
-    ( validateMPFSRequest
+    ( validateMPFSRequestTemplate
+    , validateMPFSRequest
     , ValidationResult (..)
     ) where
 
-import Network.HTTP.Client (Request)
-import Servant.Client (ClientM)
+import Core.Types (Key (..), PublicKeyHash, Username)
+import MPFS.Types (MPFSOperation (..), MPFSRequest)
+import Oracle.Github.ListPublicKeys
+    ( PublicKeyValidation (..)
+    , inspectPublicKey
+    )
+import Text.JSON.Canonical
+    ( FromJSON (..)
+    , JSValue (..)
+    , ReportSchemaErrors (..)
+    )
+import User.Types (RegisterPublicKey (..), UnregisterPublicKey (..))
+
+import MPFS.Types qualified as MPFS
 
 data ValidationResult
     = Validated
@@ -15,7 +29,50 @@ data ValidationResult
     | NotEvaluated
     deriving (Eq, Show)
 
+instance ReportSchemaErrors IO where
+    expected expct (Just got) =
+        fail
+            $ "Expected: "
+                ++ expct
+                ++ ", but got: "
+                ++ got
+    expected expct Nothing = fail $ "Expected: " ++ expct
+
+parseRegisterPubKey :: JSValue -> IO RegisterPublicKey
+parseRegisterPubKey = fromJSON @_ @RegisterPublicKey
+
+parseUnregisterPubKey :: JSValue -> IO UnregisterPublicKey
+parseUnregisterPubKey = fromJSON @_ @UnregisterPublicKey
+
+validateMPFSRequestTemplate
+    :: MPFSRequest
+    -> (Username -> PublicKeyHash -> IO PublicKeyValidation)
+    -> IO ValidationResult
+validateMPFSRequestTemplate request validatePubKey = do
+    let (Key key) = MPFS.key $ MPFS.change request
+        op = MPFS.operation $ MPFS.change request
+    case op of
+        InsertOp -> do
+            RegisterPublicKey{username, pubkeyhash} <- parseRegisterPubKey key
+            validationRes <- validatePubKey username pubkeyhash
+            case validationRes of
+                PublicKeyValidated ->
+                    pure Validated
+                _ ->
+                    pure NotValidated
+        DeleteOp -> do
+            UnregisterPublicKey{username, pubkeyhash} <- parseUnregisterPubKey key
+            validationRes <- validatePubKey username pubkeyhash
+            case validationRes of
+                PublicKeyValidated ->
+                    pure Validated
+                _ ->
+                    pure NotValidated
+        _ ->
+            pure NotEvaluated
+
 validateMPFSRequest
-    :: Request
-    -> ClientM ValidationResult
-validateMPFSRequest _request = pure NotEvaluated
+    :: MPFSRequest
+    -> IO ValidationResult
+validateMPFSRequest request =
+    validateMPFSRequestTemplate request inspectPublicKey

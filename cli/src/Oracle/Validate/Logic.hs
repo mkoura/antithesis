40b4d9699b9710bb0012b001c3814eca9a93f614
Pawel Jakubas <jakubas.pawel@gmail.com>
2025-07-07 06:33:35 +0200

    adjust oracle validate logic to have it working

--- cli/src/Oracle/Validate/Logic.hs
+++ cli/src/Oracle/Validate/Logic.hs
@@ -6,33 +6,50 @@ module Oracle.Validate.Logic
     , validateRequest
     ) where
 
-import Core.Types (RequestRefId)
+import Control.Monad.IO.Class (liftIO)
+import Core.Types (Change (..), Key (..), Platform (..), RequestRefId)
 import Lib.JSON
     ( stringJSON
     )
 import Oracle.Types (Request (..), RequestZoo (..))
 import Servant.Client (ClientM)
 import Text.JSON.Canonical.Class (ToJSON (..))
+import User.Types (RegisterUserKey (..))
+
+import qualified Oracle.Github.ListPublicKeys as Github
 
 data ValidationResult
     = Validated
-    | NotValidated
-    | CannotValidate
+    | NotValidated String
+    | CannotValidate String
     | NotEvaluated
     deriving (Eq, Show)
 
 instance Monad m => ToJSON m ValidationResult where
     toJSON = \case
         Validated -> stringJSON "validated"
-        NotValidated -> stringJSON "not validated"
-        CannotValidate -> stringJSON "cannot validate"
+        NotValidated reason -> stringJSON $ "not validated: " <> reason
+        CannotValidate reason -> stringJSON $ "cannot validate: " <> reason
         NotEvaluated -> stringJSON "not evaluated"
 
 validateRequest
     :: RequestZoo
     -> ClientM (RequestRefId, ValidationResult)
-validateRequest (RegisterUserRequest (Request refId _owner _change)) =
-    pure (refId, NotEvaluated)
+validateRequest (RegisterUserRequest (Request refId _owner (Change k _v))) = do
+    res <- case k of
+        Key (RegisterUserKey {platform,username,pubkeyhash}) ->
+            case platform of
+                Platform "github" -> do
+                    validationRes <- liftIO $ Github.inspectPublicKey username pubkeyhash
+                    if validationRes == Github.PublicKeyValidated then
+                        pure Validated
+                    else
+                        pure $ NotValidated (Github.emitPublicKeyMsg validationRes)
+                Platform other ->
+                    pure $ CannotValidate "expecting github platform as we are validating only this at this moment"
+        _ ->
+            pure $ CannotValidate "expecting RegisterUserKey"
+    pure (refId, res)
 validateRequest (UnregisterUserRequest (Request refId _owner _change)) =
     pure (refId, NotEvaluated)
 validateRequest (RegisterRoleRequest (Request refId _owner _change)) =

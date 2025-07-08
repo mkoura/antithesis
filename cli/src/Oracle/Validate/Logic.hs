fd6604a8e1aa57015979f9c17031e5e4ea16818f
paolino <paolo.veronelli@gmail.com>
2025-07-08 12:39:05 +0100

    Fix just format

--- cli/src/Oracle/Validate/Logic.hs
+++ cli/src/Oracle/Validate/Logic.hs
@@ -7,7 +7,15 @@ module Oracle.Validate.Logic
     ) where
 
 import Control.Monad.IO.Class (liftIO)
-import Core.Types (Change (..), Key (..), Platform (..), PublicKeyHash (..), RequestRefId, TokenId, Username (..))
+import Core.Types
+    ( Change (..)
+    , Key (..)
+    , Platform (..)
+    , PublicKeyHash (..)
+    , RequestRefId
+    , TokenId
+    , Username (..)
+    )
 import Lib.JSON
     ( stringJSON
     )
@@ -16,11 +24,11 @@ import MPFS.API
     )
 import Oracle.Types (Request (..), RequestZoo (..))
 import Servant.Client (ClientM)
-import Text.JSON.Canonical (JSValue(..),toJSString)
+import Text.JSON.Canonical (JSValue (..), toJSString)
 import Text.JSON.Canonical.Class (ToJSON (..))
 import User.Types (RegisterUserKey (..))
 
-import qualified Oracle.Github.ListPublicKeys as Github
+import Oracle.Github.ListPublicKeys qualified as Github
 
 data ValidationResult
     = Validated
@@ -37,7 +45,7 @@ instance Monad m => ToJSON m ValidationResult where
         NotEvaluated -> stringJSON "not evaluated"
 
 instance MonadFail ClientM where
-  fail = error
+    fail = error
 
 validateRequest
     :: TokenId
@@ -45,31 +53,43 @@ validateRequest
     -> ClientM (RequestRefId, ValidationResult)
 validateRequest _tk (RegisterUserRequest (Request refId _owner (Change k _v))) = do
     res <- case k of
-        Key (RegisterUserKey {platform,username,pubkeyhash}) ->
+        Key (RegisterUserKey{platform, username, pubkeyhash}) ->
             case platform of
                 Platform "github" -> do
                     validationRes <- liftIO $ Github.inspectPublicKey username pubkeyhash
-                    if validationRes == Github.PublicKeyValidated then
-                        pure Validated
-                    else
-                        pure $ NotValidated (Github.emitPublicKeyMsg validationRes)
+                    if validationRes == Github.PublicKeyValidated
+                        then
+                            pure Validated
+                        else
+                            pure $ NotValidated (Github.emitPublicKeyMsg validationRes)
                 Platform _other ->
-                    pure $ CannotValidate "expecting github platform as we are validating only this at this moment"
+                    pure
+                        $ CannotValidate
+                            "expecting github platform as we are validating only this at this moment"
     pure (refId, res)
 validateRequest tk (UnregisterUserRequest (Request refId _owner (Change k _v))) = do
     JSObject facts <- getTokenFacts tk
-    let Key (RegisterUserKey (Platform platform) (Username username) (PublicKeyHash pubkeyhash) ) = k
-    let expEntry = ("key", JSObject
-            [ ("platform", JSString $ toJSString platform)
-            , ("publickeyhash", JSString $ toJSString pubkeyhash )
-            , ("type", JSString $ toJSString "register-user")
-            , ("user", JSString $ toJSString username)
-            ])
+    let Key
+            ( RegisterUserKey
+                    (Platform platform)
+                    (Username username)
+                    (PublicKeyHash pubkeyhash)
+                ) = k
+    let expEntry =
+            ( "key"
+            , JSObject
+                [ ("platform", JSString $ toJSString platform)
+                , ("publickeyhash", JSString $ toJSString pubkeyhash)
+                , ("type", JSString $ toJSString "register-user")
+                , ("user", JSString $ toJSString username)
+                ]
+            )
     let findRes = filter (== expEntry) facts
-    if null findRes then
-        pure (refId, NotValidated "no mirror user registration fact found")
-    else
-        pure (refId, Validated)
+    if null findRes
+        then
+            pure (refId, NotValidated "no mirror user registration fact found")
+        else
+            pure (refId, Validated)
 validateRequest _tk (RegisterRoleRequest (Request refId _owner _change)) =
     pure (refId, NotEvaluated)
 validateRequest _tk (UnregisterRoleRequest (Request refId _owner _change)) =

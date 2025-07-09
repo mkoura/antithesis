727a9c136ab271d354dccda682f792fa4a7b49cd
paolino <paolo.veronelli@gmail.com>
2025-07-09 15:31:05 +0100

    Drive bash scenarios from haskell testsuite

---
+++ cli/test/E2ESpec.hs
@@ -0,0 +1,26 @@
+module E2ESpec
+    ( spec
+    ) where
+
+import System.Exit (ExitCode (..))
+import System.Process (readProcessWithExitCode)
+import Test.Hspec (Spec, describe, it)
+
+runScenario :: FilePath -> IO ()
+runScenario script = do
+    let scriptFile = "test/scenarios/" ++ script
+    (code, _, sterr) <- readProcessWithExitCode scriptFile [] ""
+    case code of
+        ExitSuccess -> return ()
+        _ ->
+            error $ "Scenario failed: " ++ scriptFile ++ "\n" ++ sterr
+
+spec :: Spec
+spec = do
+    describe "E2E tests" $ do
+        describe "should prove that agent"
+            $ it "can to reject a test-run"
+            $ runScenario "rejectATestRun.sh"
+        describe "should prove that agent"
+            $ it "can to accept a test-run"
+            $ runScenario "acceptATestRun.sh"

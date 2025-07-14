5190b8d552da8140f58b35003150ec65c8887709
paolino <paolo.veronelli@gmail.com>
2025-07-14 14:59:52 +0100

    Add signatureGen

---
+++ cli/test-lib/Test/QuickCheck/Crypton.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Test.QuickCheck.Crypton
+    ( signatureGen
+    )
+where
+
+import Crypto.Error (CryptoFailable (..))
+import Crypto.PubKey.Ed25519 (generateSecretKey)
+import Crypto.PubKey.Ed25519 qualified as Ed25519
+import Crypto.Random (MonadRandom (..))
+import Data.ByteArray qualified as BA
+import Data.ByteString.Char8 qualified as B
+import Data.Word (Word8)
+import Test.QuickCheck
+    ( Gen
+    , choose
+    , vectorOf
+    )
+
+instance MonadRandom Gen where
+    getRandomBytes n = do
+        bytes <- vectorOf n (choose (0, 255) :: Gen Word8)
+        return $ BA.pack bytes
+
+signatureGen
+    :: Gen
+        ( String -> Ed25519.Signature
+        , String -> Ed25519.Signature -> Bool
+        )
+signatureGen = do
+    private <- generateSecretKey
+    let public = Ed25519.toPublic private
+        sign message =
+            let signature = Ed25519.sign private public $ B.pack message
+            in  case Ed25519.signature signature of
+                    CryptoFailed err -> error $ "Invalid signature: " ++ show err
+                    CryptoPassed sig -> sig
+    return (sign, Ed25519.verify public . B.pack)

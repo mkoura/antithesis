7619feb68c5b85f304bdcd6a22dde6301ca14078
paolino <paolo.veronelli@gmail.com>
2025-07-17 16:50:05 +0100

    Add id field to result to `anti agent query`

--- cli/src/Core/Types/Fact.hs
+++ cli/src/Core/Types/Fact.hs
@@ -2,10 +2,17 @@
 
 module Core.Types.Fact
     ( Fact (..)
+    , keyHash
     , parseFacts
     , JSFact
     ) where
 
+import Crypto.Hash (Blake2b_256, Digest)
+import Crypto.Hash qualified as Hash
+import Data.ByteArray qualified as BA
+import Data.ByteString.Base16 qualified as Base16
+import Data.ByteString.Char8 qualified as B
+import Data.ByteString.Lazy qualified as BL
 import Data.Maybe (fromMaybe, mapMaybe)
 import Lib.JSON
     ( object
@@ -18,6 +25,7 @@ import Text.JSON.Canonical
     , JSValue (..)
     , ReportSchemaErrors (..)
     , ToJSON (..)
+    , renderCanonicalJSON
     )
 
 data Fact k v = Fact
@@ -26,6 +34,16 @@ data Fact k v = Fact
     }
     deriving (Eq, Show)
 
+keyHash :: (ToJSON m k, Monad m) => k -> m String
+keyHash key = do
+    keyJ <- toJSON key
+    pure
+        $ B.unpack
+        $ Base16.encode
+        $ BA.convert @(Digest Blake2b_256)
+        $ Hash.hash
+        $ BL.toStrict
+        $ renderCanonicalJSON keyJ
 instance
     ( ReportSchemaErrors m
     , FromJSON m k
@@ -39,10 +57,13 @@ instance
         pure $ Fact key value
 
 instance (Monad m, ToJSON m k, ToJSON m v) => ToJSON m (Fact k v) where
-    toJSON (Fact key value) =
+    toJSON (Fact key value) = do
+        keyJ <- toJSON key
+        idJ <- keyHash key
         object
-            [ "key" .= key
+            [ "key" .= keyJ
             , "value" .= value
+            , "id" .= idJ
             ]
 
 type JSFact = Fact JSValue JSValue

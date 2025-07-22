718b851895cafd4cdf1f59f61cfe7cc91298d0cb
paolino <paolo.veronelli@gmail.com>
2025-07-22 09:45:44 +0100

    Add validation against simple presence/absence of keys in the facts (register user)

--- cli/src/Oracle/Validate/Requests/RegisterUser.hs
+++ cli/src/Oracle/Validate/Requests/RegisterUser.hs
@@ -11,49 +11,66 @@ import Core.Types.Fact (Fact (..))
 import Core.Types.Operation (Op (..))
 import Data.List (find)
 import Oracle.Validate.Types (ValidationResult (..))
-import Servant.Client (ClientM)
 import User.Types
     ( RegisterUserKey (..)
     )
-import Validation (Validation (..))
+import Validation
+    ( Validation (..)
+    , deleteValidation
+    , insertValidation
+    )
 import Validation.RegisterUser qualified as Github
 
 validateRegisterUser
-    :: Validation ClientM
+    :: Monad m
+    => Validation m
     -> Change RegisterUserKey (OpI ())
-    -> ClientM ValidationResult
-validateRegisterUser Validation{githubUserPublicKeys} (Change k _v) = case k of
-    Key (RegisterUserKey{platform, username, pubkeyhash}) ->
-        case platform of
-            Platform "github" -> do
-                validationRes <- githubUserPublicKeys username pubkeyhash
-                if validationRes == Github.PublicKeyValidated
-                    then
-                        pure Validated
-                    else
-                        pure $ NotValidated (Github.emitPublicKeyMsg validationRes)
-            Platform _other ->
-                pure
-                    $ CannotValidate
-                        "expecting github platform as we are validating only this at this moment"
+    -> m ValidationResult
+validateRegisterUser
+    validation@Validation{githubUserPublicKeys}
+    change@(Change k _v) = do
+        insertionValidation <- insertValidation validation change
+        if insertionValidation /= Validated
+            then pure insertionValidation
+            else case k of
+                Key (RegisterUserKey{platform, username, pubkeyhash}) ->
+                    case platform of
+                        Platform "github" -> do
+                            validationRes <- githubUserPublicKeys username pubkeyhash
+                            if validationRes == Github.PublicKeyValidated
+                                then
+                                    pure Validated
+                                else
+                                    pure $ NotValidated (Github.emitPublicKeyMsg validationRes)
+                        Platform _other ->
+                            pure
+                                $ CannotValidate
+                                    "expecting github platform as we are validating only this at this moment"
 
 validateUnregisterUser
-    :: Validation ClientM
+    :: Monad m
+    => Validation m
     -> Change RegisterUserKey (OpD ())
-    -> ClientM ValidationResult
-validateUnregisterUser Validation{mpfsGetFacts} (Change (Key k) _v) = do
-    facts <- mpfsGetFacts
-    let registration = find (\(Fact k' ()) -> k' == k) facts
-    if null registration
-        then
-            pure
-                $ NotValidated
-                $ "no registration for platform '"
-                    <> show (platform k)
-                    <> "' and user '"
-                    <> show (username k)
-                    <> "' and public key hash '"
-                    <> show (pubkeyhash k)
-                    <> "' found"
-        else
-            pure Validated
+    -> m ValidationResult
+validateUnregisterUser
+    validation@Validation{mpfsGetFacts}
+    change@(Change (Key k) _v) = do
+        deletionValidation <- deleteValidation validation change
+        if deletionValidation /= Validated
+            then pure deletionValidation
+            else do
+                facts <- mpfsGetFacts
+                let registration = find (\(Fact k' ()) -> k' == k) facts
+                if null registration
+                    then
+                        pure
+                            $ NotValidated
+                            $ "no registration for platform '"
+                                <> show (platform k)
+                                <> "' and user '"
+                                <> show (username k)
+                                <> "' and public key hash '"
+                                <> show (pubkeyhash k)
+                                <> "' found"
+                    else
+                        pure Validated

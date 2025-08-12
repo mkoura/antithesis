a7ba16ed9419043e2f90c436521146d1c8b67168
paolino <paolo.veronelli@gmail.com>
2025-08-12 20:45:43 +0100

    Implement ManageWhiteList

--- cli/src/Oracle/Validate/Requests/ManageWhiteList.hs
+++ cli/src/Oracle/Validate/Requests/ManageWhiteList.hs
@@ -1,28 +1,75 @@
-module Oracle.Validate.Requests.ManageWhiteList (validateAddWhiteListed, validateRemoveWhiteListed)
+module Oracle.Validate.Requests.ManageWhiteList
+    ( validateAddWhiteListed
+    , validateRemoveWhiteListed
+    , UpdateWhiteListFailure (..)
+    )
 where
 
-import Core.Types.Basic (Platform, Repository)
-import Core.Types.Change (Change)
+import Control.Monad (void)
+import Control.Monad.Trans.Class (MonadTrans (..))
+import Core.Types.Basic (Platform (..), Repository)
+import Core.Types.Change (Change (..), Key (..))
 import Core.Types.Operation (Op (..))
-import Oracle.Validate.Types (Validate, Validated (..))
-import User.Agent.Types (WhiteListKey)
-import Validation (Validation)
+import Lib.GitHub qualified as Github
+import Lib.JSON.Canonical.Extra (object, (.=))
+import Oracle.Validate.Types
+    ( Validate
+    , Validated (..)
+    , mapFailure
+    , throwFalse
+    , throwLeft
+    )
+import Text.JSON.Canonical (ToJSON (..))
+import User.Agent.Types (WhiteListKey (..))
+import Validation
+    ( KeyFailure
+    , Validation (..)
+    , deleteValidation
+    , insertValidation
+    )
 
 data UpdateWhiteListFailure
-    = PlatformMissing Platform
-    | RepositoryMissing Repository
+    = WhiteListPlatformUnsupported Platform
+    | WhiteListRepositoryNotInThePlatform Repository
+    | WhiteListRepositoryKeyValidation KeyFailure
+    | WhiteListGithubFailed Github.GithubResponseStatusCodeError
     deriving (Show, Eq)
+
+instance Monad m => ToJSON m UpdateWhiteListFailure where
+    toJSON (WhiteListPlatformUnsupported platform) =
+        object ["error" .= ("Platform is missing: " ++ show platform)]
+    toJSON (WhiteListRepositoryNotInThePlatform repo) =
+        object
+            ["error" .= ("Repository is not in the platform: " ++ show repo)]
+    toJSON (WhiteListRepositoryKeyValidation keyFailure) =
+        object
+            ["error" .= ("Repository key validation failed: " ++ show keyFailure)]
+    toJSON (WhiteListGithubFailed err) =
+        object ["error" .= ("GitHub error: " ++ show err)]
 
 validateAddWhiteListed
     :: Monad m
     => Validation m
     -> Change WhiteListKey (OpI ())
     -> Validate UpdateWhiteListFailure m Validated
-validateAddWhiteListed _ _ = pure Validated
+validateAddWhiteListed
+    v@Validation{githubRepositoryExists}
+    c@(Change (Key (WhiteListKey platform repo)) _) = do
+        mapFailure WhiteListRepositoryKeyValidation $ insertValidation v c
+        void
+            $ throwFalse (platform == Platform "github")
+            $ WhiteListPlatformUnsupported platform
+        eexists <- lift $ githubRepositoryExists repo
+        exist <- throwLeft WhiteListGithubFailed eexists
+        throwFalse exist
+            $ WhiteListRepositoryNotInThePlatform repo
 
 validateRemoveWhiteListed
     :: Monad m
     => Validation m
     -> Change WhiteListKey (OpD ())
     -> Validate UpdateWhiteListFailure m Validated
-validateRemoveWhiteListed _ _ = pure Validated
+validateRemoveWhiteListed v c@(Change (Key (WhiteListKey platform _repo)) _) = do
+    mapFailure WhiteListRepositoryKeyValidation $ deleteValidation v c
+    throwFalse (platform == Platform "github")
+        $ WhiteListPlatformUnsupported platform

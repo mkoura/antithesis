85fa9fd179af457a5533771a439b6cda97fbf185
Arnaud Bailly <arnaud@pankzsoft.com>
2025-08-19 10:40:34 +0200

    wip: make chain sync client compile
    required fiddling with dependencies, cabal hell, hackage, CHaP and
    what not.

--- adversary/src/Adversary/ChainSync.hs
+++ adversary/src/Adversary/ChainSync.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE BangPatterns         #-}
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE FlexibleContexts     #-}
 {-# LANGUAGE FlexibleInstances    #-}
 {-# LANGUAGE NamedFieldPuns       #-}
@@ -44,8 +45,8 @@ import Network.TypedProtocol.Codec
 
 import Network.Mux qualified as Mx
 
-import Cardano.Network.NodeToClient
-import Cardano.Network.NodeToNode
+import Ouroboros.Network.NodeToClient
+import Ouroboros.Network.NodeToNode
 
 import Ouroboros.Network.AnchoredFragment qualified as AF
 import Ouroboros.Network.Block
@@ -70,11 +71,6 @@ import Ouroboros.Network.Protocol.BlockFetch.Codec qualified as BlockFetch
 import Ouroboros.Network.Protocol.BlockFetch.Server qualified as BlockFetch
 import Ouroboros.Network.Protocol.BlockFetch.Type qualified as BlockFetch
 
-import Ouroboros.Network.BlockFetch
-import Ouroboros.Network.BlockFetch.Client
-import Ouroboros.Network.BlockFetch.ClientRegistry (FetchClientRegistry (..))
-import Ouroboros.Network.BlockFetch.ConsensusInterface (ChainSelStarvation (..),
-           initialWithFingerprint)
 import Ouroboros.Network.DeltaQ (defaultGSV)
 
 import Ouroboros.Network.Server.Simple qualified as Server.Simple
@@ -112,3 +108,134 @@ clientChainSync sockPaths maxSlotNo = withIOManager $ \iocp ->
             , codecChainSync
             , ChainSync.chainSyncClientPeer (chainSyncClient (continueForever Proxy) maxSlotNo)
             )
+
+-- TODO: provide sensible limits
+-- https://github.com/intersectmbo/ouroboros-network/issues/575
+maximumMiniProtocolLimits :: MiniProtocolLimits
+maximumMiniProtocolLimits =
+    MiniProtocolLimits {
+      maximumIngressQueue = maxBound
+    }
+
+
+--
+-- Chain sync demo
+--
+
+demoProtocol2
+  :: RunMiniProtocolWithMinimalCtx appType addr bytes m a b -- ^ chainSync
+  -> OuroborosApplicationWithMinimalCtx appType addr bytes m a b
+demoProtocol2 chainSync =
+    OuroborosApplication [
+      MiniProtocol {
+        miniProtocolNum    = MiniProtocolNum 2,
+        miniProtocolStart  = StartOnDemand,
+        miniProtocolLimits = maximumMiniProtocolLimits,
+        miniProtocolRun    = chainSync
+      }
+    ]
+
+
+chainSyncClient :: ControlMessageSTM IO
+                -> Maybe SlotNo
+                -> ChainSync.ChainSyncClient
+                     BlockHeader (Point BlockHeader) (Point BlockHeader) IO ()
+chainSyncClient controlMessageSTM maxSlotNo =
+    ChainSync.ChainSyncClient $ do
+      curvar   <- newTVarIO genesisAnchoredFragment
+      chainvar <- newTVarIO genesisAnchoredFragment
+      case chainSyncClient' controlMessageSTM maxSlotNo  nullTracer curvar chainvar of
+        ChainSync.ChainSyncClient k -> k
+
+chainSyncClient' :: ControlMessageSTM IO
+                 -> Maybe SlotNo
+                 -> Tracer IO (Point BlockHeader, Point BlockHeader)
+                 -> StrictTVar IO (AF.AnchoredFragment BlockHeader)
+                 -> StrictTVar IO (AF.AnchoredFragment BlockHeader)
+                 -> ChainSync.ChainSyncClient
+                      BlockHeader (Point BlockHeader) (Point BlockHeader) IO ()
+chainSyncClient' controlMessageSTM _maxSlotNo syncTracer _currentChainVar candidateChainVar =
+    ChainSync.ChainSyncClient (return requestNext)
+  where
+    requestNext :: ChainSync.ClientStIdle
+                     BlockHeader (Point BlockHeader) (Point BlockHeader) IO ()
+    requestNext =
+      ChainSync.SendMsgRequestNext
+        (pure ())   -- on MsgAwaitReply; could trace
+        handleNext
+
+    terminate :: ChainSync.ClientStIdle
+                     BlockHeader (Point BlockHeader) (Point BlockHeader) IO ()
+    terminate = ChainSync.SendMsgDone ()
+
+    handleNext :: ChainSync.ClientStNext
+                    BlockHeader (Point BlockHeader) (Point BlockHeader) IO ()
+    handleNext =
+      ChainSync.ClientStNext {
+        ChainSync.recvMsgRollForward  = \header _pHead ->
+          ChainSync.ChainSyncClient $ do
+            addBlock header
+            cm <- atomically controlMessageSTM
+            return $ case cm of
+              Terminate -> terminate
+              _         -> requestNext
+
+      , ChainSync.recvMsgRollBackward = \pIntersect _pHead ->
+          ChainSync.ChainSyncClient $ do
+            rollback pIntersect
+            cm <- atomically controlMessageSTM
+            return $ case cm of
+              Terminate -> terminate
+              _         -> requestNext
+      }
+
+    addBlock :: BlockHeader -> IO ()
+    addBlock b = do
+        chain <- atomically $ do
+          chain <- readTVar candidateChainVar
+          let !chain' = shiftAnchoredFragment 50 b chain
+          writeTVar candidateChainVar chain'
+          return chain'
+        traceWith syncTracer (AF.lastPoint chain, AF.headPoint chain)
+
+    rollback :: Point BlockHeader -> IO ()
+    rollback p = atomically $ do
+        chain <- readTVar candidateChainVar
+        -- we do not handle rollback failure in this demo
+        let (Just !chain') = AF.rollback p chain
+        writeTVar candidateChainVar chain'
+    {-
+    notTooFarAhead = atomically $ do
+        currentChain   <- readTVar currentChainVar
+        candidateChain <- readTVar candidateChainVar
+        check $ case (AF.headBlockNo currentChain,
+                      AF.headBlockNo candidateChain) of
+                  (Just bn, Just bn') -> bn' < bn + 5
+                  _                   -> True
+    -}
+
+
+
+codecChainSync :: ( CBOR.Serialise block
+                  , CBOR.Serialise point
+                  , CBOR.Serialise tip
+                  )
+               => Codec (ChainSync.ChainSync block point tip)
+                        CBOR.DeserialiseFailure
+                        IO LBS.ByteString
+codecChainSync =
+    ChainSync.codecChainSync
+      CBOR.encode CBOR.decode
+      CBOR.encode CBOR.decode
+      CBOR.encode CBOR.decode
+
+genesisAnchoredFragment :: AF.AnchoredFragment BlockHeader
+genesisAnchoredFragment = AF.Empty AF.AnchorGenesis
+
+shiftAnchoredFragment :: HasHeader block
+                      => Int
+                      -> block
+                      -> AF.AnchoredFragment block
+                      -> AF.AnchoredFragment block
+shiftAnchoredFragment n b af =
+  AF.anchorNewest (fromIntegral (AF.length af - n)) af AF.:> b

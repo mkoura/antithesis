55955e6e1474054905471b74e69eb22e28f6cf2e
Johannes Lund <anviking@me.com>
2025-08-19 12:14:09 +0200

    Add nix shell for tracer-sidecar

---
+++ compose/tracer-sidecar/nix/project.nix
@@ -0,0 +1,40 @@
+{ CHaP, indexState, pkgs, cardano-cli, ... }:
+
+let
+  shell = { pkgs, ... }: {
+    tools = {
+      cabal = { index-state = indexState; };
+      cabal-fmt = { index-state = indexState; };
+      haskell-language-server = { index-state = indexState; };
+      hoogle = { index-state = indexState; };
+      fourmolu = { index-state = indexState; };
+      hlint = { index-state = indexState; };
+    };
+    withHoogle = true;
+    buildInputs = [
+      pkgs.gitAndTools.git
+      pkgs.just
+      pkgs.nixfmt-classic
+
+    ];
+    shellHook = ''
+      echo "Entering shell for tracer-sidecar development"
+    '';
+  };
+
+  mkProject = ctx@{ lib, pkgs, ... }: {
+    name = "tracer-sidecar";
+    src = ./..;
+    compiler-nix-name = "ghc984";
+    shell = shell { inherit pkgs; };
+
+    modules = [ ];
+    inputMap = { "https://chap.intersectmbo.org/" = CHaP; };
+  };
+  project = pkgs.haskell-nix.cabalProject' mkProject;
+
+in {
+  devShells.default = project.shell;
+  packages.tracer-sidecar = project.hsPkgs.tracer-sidecar.components.exes.tracer-sidecar;
+  inherit project;
+}

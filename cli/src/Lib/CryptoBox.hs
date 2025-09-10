c6a5b15c3dcfe47351db8ffa485c039246a8a4ab
paolino <paolo.veronelli@gmail.com>
2025-09-10 18:41:47 +0100

    [cli] feat: add crypto-box

---
+++ cli/src/Lib/CryptoBox.hs
@@ -0,0 +1,44 @@
+module Lib.CryptoBox
+    ( Nonce192 (..)
+    , encrypt
+    , decrypt
+    )
+where
+
+import Crypto.Box (create, open)
+import Crypto.PubKey.Ed25519 (PublicKey, SecretKey)
+import Data.ByteString (ByteString)
+import Lib.EdToCurve (convertPublicKey, convertSecretKey)
+
+newtype Nonce192 = Nonce192 ByteString
+    deriving (Show, Eq)
+
+encrypt
+    :: PublicKey
+    -- ^ Recipient's public key
+    -> SecretKey
+    -- ^ Sender's secret key
+    -> ByteString
+    -- ^ Plaintext message
+    -> Nonce192
+    -- ^ nonce
+    -> Either String ByteString
+encrypt pk sk msg (Nonce192 nonce) = do
+    pk' <- convertPublicKey pk
+    sk' <- convertSecretKey sk
+    Right $ create msg nonce pk' sk'
+
+decrypt
+    :: PublicKey
+    -- ^ Sender's public key
+    -> SecretKey
+    -- ^ Recipient's secret key
+    -> ByteString
+    -- ^ Ciphertext message
+    -> Nonce192
+    -- ^ nonce
+    -> Either String (Maybe ByteString)
+decrypt pk sk msg (Nonce192 nonce) = do
+    pk' <- convertPublicKey pk
+    sk' <- convertSecretKey sk
+    Right $ open msg nonce pk' sk'
---
+++ cli/test/Lib/CryptoBoxSpec.hs
@@ -0,0 +1,66 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Lib.CryptoBoxSpec
+    ( spec
+    )
+where
+
+import Crypto.PubKey.Ed25519
+    ( PublicKey
+    , SecretKey
+    , generateSecretKey
+    , toPublic
+    )
+import Crypto.Random (MonadRandom (..))
+import Data.ByteArray (convert)
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as B
+import Data.Functor ((<&>))
+import Data.Word (Word8)
+import Lib.CryptoBox (Nonce192 (..), decrypt, encrypt)
+import Test.Hspec (Spec, describe, expectationFailure, shouldBe)
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck
+    ( Arbitrary (arbitrary)
+    , Gen
+    , forAllBlind
+    , listOf
+    , vectorOf
+    , withMaxSuccess
+    )
+
+data Params = Params
+    { agentPk :: PublicKey
+    , agentSk :: SecretKey
+    , userPk :: PublicKey
+    , userSk :: SecretKey
+    , msg :: ByteString
+    , nonce :: Nonce192
+    }
+
+instance MonadRandom Gen where
+    getRandomBytes n = convert . B.pack <$> vectorOf n (arbitrary :: Gen Word8)
+
+generateParams :: Gen Params
+generateParams = do
+    agentSk <- generateSecretKey
+    let agentPk = toPublic agentSk
+    userSk <- generateSecretKey
+    let userPk = toPublic userSk
+    msg <- listOf (arbitrary :: Gen Word8) <&> B.pack
+    nonce <- vectorOf (192 `div` 8) (arbitrary :: Gen Word8) <&> B.pack
+    pure
+        $ Params{agentPk, agentSk, userPk, userSk, msg, nonce = Nonce192 nonce}
+
+spec :: Spec
+spec = describe "CryptoBox"
+    $ prop "can create and open"
+    $ withMaxSuccess 10000
+    $ forAllBlind generateParams
+    $ \Params{agentSk, agentPk, userPk, userSk, msg, nonce} ->
+        case encrypt userPk agentSk msg nonce of
+            Left err -> expectationFailure err
+            Right encrypted ->
+                case decrypt agentPk userSk encrypted nonce of
+                    Left err -> expectationFailure err
+                    Right decrypted -> decrypted `shouldBe` Just msg

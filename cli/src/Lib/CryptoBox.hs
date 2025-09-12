472fb7678b0e8406e7bb1add0dc96e87e7f8cbf1
paolino <paolo.veronelli@gmail.com>
2025-09-12 16:32:06 +0100

    [cli] refact: expose mkNonce from crypto box

--- cli/src/Lib/CryptoBox.hs
+++ cli/src/Lib/CryptoBox.hs
@@ -2,16 +2,23 @@ module Lib.CryptoBox
     ( Nonce192 (..)
     , encrypt
     , decrypt
+    , mkNonce
     )
 where
 
 import Crypto.Box (create, open)
 import Crypto.PubKey.Ed25519 (PublicKey, SecretKey)
 import Data.ByteString (ByteString)
+import Data.ByteString qualified as B
 import Lib.EdToCurve (convertPublicKey, convertSecretKey)
 
 newtype Nonce192 = Nonce192 ByteString
     deriving (Show, Eq)
+
+mkNonce :: ByteString -> Maybe Nonce192
+mkNonce bs
+    | B.length bs >= 24 = Just (Nonce192 $ B.take 24 bs)
+    | otherwise = Nothing
 
 encrypt
     :: PublicKey

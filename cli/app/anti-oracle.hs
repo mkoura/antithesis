aa4ab4b253970d968163e7ddf2238417d101b597
paolino <paolo.veronelli@gmail.com>
2025-09-16 20:01:39 +0100

    [cli] fix: force line buffering in anti-oracle executable

--- cli/app/anti-oracle.hs
+++ cli/app/anti-oracle.hs
@@ -1,4 +1,9 @@
 import Oracle.Process (oracleProcess, parseArgs)
+import System.IO (BufferMode (..), hSetBuffering, stdin, stdout)
 
 main :: IO ()
-main = parseArgs >>= oracleProcess
+main = do
+    hSetBuffering stdin LineBuffering
+    hSetBuffering stdout LineBuffering
+    args <- parseArgs
+    oracleProcess args
